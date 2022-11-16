156282b4d2bb00b56b6a83329f027e7d51daa8c2
Alian713 <divy1211.dc@gmail.com>
2022-11-17 02:09:27 +0800

    updated cyclean file

--- src/cyclean.py
+++ src/cyclean.py
@@ -3,5 +3,5 @@ from pathlib import Path
 root_directory = Path(__file__).parent
 source_directory = root_directory
 
-for file in [file for file in source_directory.rglob('*.*') if file.suffix in {'.c', '.pyd', '.html'}]:
+for file in [file for file in source_directory.rglob('*.*') if file.suffix in {'.c', '.pyd', '.so', '.html'}]:
     file.unlink()
