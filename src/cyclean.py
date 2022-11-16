da574c554028a318aef2c7567e60d3e84eff098d
Alian713 <divy1211.dc@gmail.com>
2022-11-17 01:11:29 +0800

    Added clean file

---
+++ src/cyclean.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+root_directory = Path(__file__).parent
+source_directory = root_directory
+
+for file in [file for file in source_directory.rglob('*.*') if file.suffix in {'.c', '.pyd', '.html'}]:
+    file.unlink()
